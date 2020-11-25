59535e91d64cefbd787f45d223507fc1f92cf3d5
gdarold <gadarold@gmail.com>
2020-11-24 22:15:40 -0300

    exercicio 1

---
+++ Exercicio01/main.js
@@ -0,0 +1,14 @@
+const nome = "Gabriel";
+const idade = 17;
+
+console.log(nome, idade);
+
+console.log(typeof nome);
+
+console.log(typeof idade);
+
+if (idade >= 18) {
+    console.log("maior de idade")
+} else {
+    console.log("menor de idade")
+}
