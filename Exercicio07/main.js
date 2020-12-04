f074e52fee47d755c4c81645c88a9755bc9cebd4
gdarold <gadarold@gmail.com>
2020-12-03 21:58:20 -0300

    exercicio 7 ok

--- Exercicio07/main.js
+++ Exercicio07/main.js
@@ -6,12 +6,13 @@ function calculo() {
 
     var meses = new Array("ano", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
 
-    var dia = num1.substring(8, 10);
+    var dia = parseInt(num1.substring(8, 10));
 
-    var ano = num1.substring(0, 4);
+    var ano = parseInt(num1.substring(0, 4));
 
-    var mes = num1.substring(5, 7);
-    // window.alert(typeof mes);
+    var mes = parseInt(num1.substring(5, 7));
+
+
 
 
     if (dia != "" && mes != "" && ano != "") {
@@ -36,7 +37,7 @@ function validar(datas) {
         return true;
     } else {
 
-        window.alert('passou nda')
+
         return false;
     }
 
