678366b956ee55738c71bc4328837f6f1a3128ea
gdarold <gadarold@gmail.com>
2020-12-05 23:04:39 -0300

    exercicio 10 ok

--- Exercicio10/main.js
+++ Exercicio10/main.js
@@ -4,4 +4,17 @@ const resultados = document.querySelector('div.resultados')
 tag.addEventListener('blur', function (evento) {
     // Continue a tarefa aqui...
 
+
+
+    var result = calcular(tag.value);
+    resultados.innerHTML += '<h3>Temperarura em Fahrenheit</h3>';
+    resultados.innerHTML += result + ' °F';
 })
+
+
+function calcular(temp) {
+
+    var resultado = ((temp * 9 / 5) + 32);
+    return resultado;
+
+}
